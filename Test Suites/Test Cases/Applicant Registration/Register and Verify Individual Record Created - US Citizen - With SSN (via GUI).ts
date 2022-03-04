b63d7139ac2c1aa843d5af26aad34aefb26659cb
Federico Cuelho <95640419+fedecl-ot@users.noreply.github.com>
2022-03-04 14:12:42 -0300

    Verify individual record tc using GUI for IMPL-1406

---
+++ Test Suites/Test Cases/Applicant Registration/Register and Verify Individual Record Created - US Citizen - With SSN (via GUI).ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Register and Verify Individual Record Created - US Citizen - With SSN (via GUI)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>dc3258fb-e6ed-4499-a14a-12b380aeb42b</testSuiteGuid>
+   <testCaseLink>
+      <guid>5ba9bffe-451d-4cc8-a9a5-aa66d189d71f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Licensure Unit User Registration/Verify Individual Record Created - US Citizen - With SSN (via API)</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
